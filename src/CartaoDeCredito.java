1181ac5080639376b4b68edf1b95d5313da46cbd
renato <rsosilva@gmail.com>
2023-08-08 15:22:53 -0300

    Finalizado desafio

---
+++ src/CartaoDeCredito.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compras> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compras compra) {
+        if(this.saldo > compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compras> getCompras() {
+        return compras;
+    }
+}
